48f3e574d28d199a7c91181afcc115894050dce5
nmar30 <nmarcisso@live.com>
2021-04-02 16:17:55 -0400

    Completed Logic

--- day-trip-generator.js
+++ day-trip-generator.js
@@ -1,5 +1,14 @@
 "use strict"
 
+//Set Initial Variables
+let destination;
+let resturant;
+let transportation;
+let entertainment;
+let ranOnce = false;
+let confirmTrip = false;
+let confirmTripBoolean = false;
+
 //Initial Arrays
 let destinationArray = ["New York","Boston","Miami","Los Angeles","Seattle"];
 let resturantArray = ["Mexican","Italian","American","Seafood","Thai"];
@@ -12,16 +21,37 @@ function randomSelector(array){
     let arrayChoice = array[randomNumber];
     return arrayChoice;
 }
+function rerunRandomSelector (name, variable, array){
+    let answer = prompt("Your current destination is " + variable + "! Would you like to rerun (Yes|No)");
+    answer = answer.toLowerCase();
+    while (answer === "yes") {
+        variable = randomSelector(array);
+        answer = prompt("Your current destination is " + variable + "! Would you like to rerun (Yes|No)");
+        answer = answer.toLowerCase();
+    }
+    return variable;
+}
 
-//Set Initial Variables
-let destination = randomSelector(destinationArray);
-let resturant = randomSelector(resturantArray);
-let transportation = randomSelector(transportationArray);
-let entertainment = randomSelector(entertainmentArray);
+//Logic
+while (confirmTripBoolean === false){
+    if (ranOnce===false){
+        destination = randomSelector(destinationArray);
+        resturant = randomSelector(resturantArray);
+        transportation = randomSelector(transportationArray);
+        entertainment = randomSelector(entertainmentArray);
+        ranOnce=true;
+    } else {
+        destination = rerunRandomSelector("destination, destinationArray);
+        resturant = rerunRandomSelector(resturant, resturantArray);
+        transportation = rerunRandomSelector(transportation, transportationArray);
+        entertainment = rerunRandomSelector(entertainment, entertainmentArray);
+        confirmTripBoolean = true;
+    }
+}
 
-//Console Initial Results
-console.log("Your Current Trip:");
+//Console Results
+console.log("Your Confirmed Trip Itenary:");
 console.log("Destination: " + destination);
 console.log("Resturant: " + resturant);
 console.log("Transportation: " + transportation);
-console.log("Entertainment :" +  entertainment);
+console.log("Entertainment: " +  entertainment);
